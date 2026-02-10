9e39530f2f8dee071ba8f7d4d30dfe9bbbf157a6
Aaron Kipf <samplethis84@gmail.com>
2026-02-10 14:44:28 -0600

    feat: starting to build components

--- src/App.js
+++ src/App.js
@@ -25,7 +25,69 @@ const initialFriends = [
 export default function App() {
   return (
     <div className="app">
-      <h1>Splitwise</h1>
+      <div className="sidebar">
+        <FriendList />
+        <AddAFriend />
+      </div>
+      <BillForm />
     </div>
+  );
+}
+
+function FriendList() {
+  const [friends, setFriends] = useState(initialFriends);
+  return (
+    <ul>
+      {initialFriends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p>{friend.balance}</p>
+      <Button />
+    </li>
+  );
+}
+
+function Button() {
+  return <button className="button">Select</button>;
+}
+
+function AddAFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👫 Friend Name</label>
+      <input type="text" />
+      <label>🌆 Image URL</label>
+      <input type="text" />
+      <button className="button">Add</button>
+    </form>
+  );
+}
+
+function BillForm() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with Sarah</h2>
+      <label>💰 Bill Value</label>
+      <input type="text" />
+      <label>🙋‍♂️ Your Expense</label>
+      <input type="text" />
+      <label>🙋‍♀️ Sarah's Expense</label>
+      <input type="text" disabled />
+      <label>💵 Who is paying the bill?</label>
+      <select>
+        <option value="you">You</option>
+        <option value="friend">Sarah</option>
+      </select>
+      <button className="button">Split Bill</button>
+    </form>
   );
 }
